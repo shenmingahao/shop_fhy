c284925a089b3093c22a156be18165fa1f8eae49
fenghaoyi <1693917011@qq.com>
2021-01-17 15:39:48 +0800

    修改上传照片接口

--- src/main/java/com/fh/shop_fhy/controller/BrandController.java
+++ src/main/java/com/fh/shop_fhy/controller/BrandController.java
@@ -2,6 +2,7 @@ package com.fh.shop_fhy.controller;
 
 import com.fh.shop_fhy.model.Brand;
 import com.fh.shop_fhy.service.BrandService;
+import com.fh.shop_fhy.utils.OssFile;
 import com.fh.shop_fhy.vo.BrandParams;
 import com.fh.shop_fhy.vo.PageResult;
 import com.fh.shop_fhy.vo.ReponseData;
@@ -78,45 +79,14 @@ public class BrandController {
     //上传照片
     @PostMapping("uploadFile")
     public Map<String , Object> upload(MultipartFile img) throws IOException {
-        Map<String , Object> map = new HashMap<String , Object>();
-        //将文件保存到 服务器的指定目录下
-        //获取项目的绝对路径
-        String realPath = request.getServletContext().getRealPath("/");
-        //java里面 file是操作文件或者文件夹的
-        String imgPath = realPath+"imgs/";
-        //先操作文件或者文件夹的操作对象
-        File f = new File(imgPath);
-        //判断文件夹是否存在
-        boolean exists = f.exists();
-        //如果不存在
-        if (exists == false) {
-            //创建文件夹  mkdirs是创建多层文件夹
-            //mkdir是创建单层文件夹  相比使用mkdirs 比 mkdir 好点
-            f.mkdirs();
-        }
-        //获取文件的名称
-        String fileName = img.getOriginalFilename();
-
-        //文件的后缀
-        String fileLast = fileName.substring(fileName.lastIndexOf("."));
-        //生成新的名称
-        String fileNewName = UUID.randomUUID().toString()+fileLast;
-        //拷贝文件
-        //创建输出流
-        FileOutputStream os = new FileOutputStream(new File(imgPath+"/"+fileNewName));
-        //获取文件的输入流
-        FileInputStream is = (FileInputStream) img.getInputStream();
-        //设置一次读取的大小
-        byte[] bts = new byte[1024*2];
-        //通过while循环读取 直到读完
-        while (is.read(bts) != -1) {
-            os.write(bts);
-        }
-        //释放资源 由内到外 关流
-        is.close();
-        os.close();
-        //返回
-        map.put("filePath", "http://localhost:8080/imgs/"+fileNewName);
+        //处理新名称
+        String originalFilename = img.getOriginalFilename();
+        //防止中文引起的错误
+        String newName= UUID.randomUUID().toString()+originalFilename.substring(originalFilename.lastIndexOf("."));
+        //存储路径
+        newName="images/"+newName;
+        Map map = new HashMap();
+        map.put("filePath" , OssFile.uploadFile(img.getInputStream(),newName));
         return map;
     }
 }
