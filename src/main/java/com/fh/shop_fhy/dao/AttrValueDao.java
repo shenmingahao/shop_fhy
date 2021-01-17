32a486a3c99d1b2d55da1a728a396ec7374c23fd
fenghaoyi <1693917011@qq.com>
2021-01-17 20:56:33 +0800

    修改属性值修改接口

--- src/main/java/com/fh/shop_fhy/dao/AttrValueDao.java
+++ src/main/java/com/fh/shop_fhy/dao/AttrValueDao.java
@@ -19,7 +19,7 @@ public interface AttrValueDao {
     @Select("select id,name,nameCH,isDel,attrId from shop_attr_value where id = #{id}")
     AttrValue huixian(Integer id);
 
-    @Update("update shop_attr_value set name=#{name},nameCH=#{nameCH},isDel=#{isDel},attrId=#{attrId} where id = #{id}")
+    @Update("update shop_attr_value set name=#{name},nameCH=#{nameCH},isDel=#{isDel} where id = #{id}")
     void updateAttrValue(AttrValue attrValue);
 
     @Update("update shop_attr_value set isDel=1 where id = #{id}")
